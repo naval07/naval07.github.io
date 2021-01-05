ae7e1905f321f09c24d6d43919d3d3e56e554e02
Emanuel <emanuelnaval07@gmail.com>
2021-01-05 15:31:25 -0500

    intentando solucionar la "animacion"

--- main.js
+++ main.js
@@ -3,16 +3,16 @@ let title = document.getElementById('title');
 title.classList.add('title');
 
 function callback(){
-    if (title.classList.contains('title1')){
-        title.classList.remove('title1');
-        title.classList.add('title');
-    }
-    if (title.classList.contains('title')){
-        title.classList.remove('title');
-        title.classList.add('title1');
-    }
+    // if (title.classList.contains('title1')){
+    //     title.classList.remove('title1');
+    //     title.classList.add('title');
+    // }
+    // if (title.classList.contains('title')){
+    //     title.classList.remove('title');
+    //     title.classList.add('title1');
+    // }
+    title.classList.remove('title');
+    title.classList.add('title1');
 }
 
-while (true){
-    setTimeout(callback,2000);
-}
+setTimeout(callback,2000);
