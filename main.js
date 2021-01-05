94b5df32430a3d221faf0bebfcfe35910b8b6802
Emanuel <emanuelnaval07@gmail.com>
2021-01-05 15:16:19 -0500

    archivos de prueba

---
+++ main.js
@@ -0,0 +1,18 @@
+let title = document.getElementById('title');
+
+title.classList.add('title1');
+
+function callback(){
+    if (title.classList.contains('title1')){
+        title.classList.remove('title1');
+        title.classList.add('title');
+    }
+    if (title.classList.contains('title')){
+        title.classList.remove('title');
+        title.classList.add('title1');
+    }
+}
+
+while (true){
+    setTimeout(callback,2000);
+}
