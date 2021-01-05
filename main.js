c7469eb08c067fa4e6a1725be92efc69b4647e3f
Emanuel <emanuelnaval07@gmail.com>
2021-01-05 16:59:02 -0500

    animacion finalizada

--- main.js
+++ main.js
@@ -1,18 +1,21 @@
-let title = document.getElementById('title');
-
+var title = document.getElementById('title');
+console.log('hfjdksh')
 title.classList.add('title');
 
-function callback(){
-    // if (title.classList.contains('title1')){
-    //     title.classList.remove('title1');
-    //     title.classList.add('title');
-    // }
-    // if (title.classList.contains('title')){
-    //     title.classList.remove('title');
-    //     title.classList.add('title1');
-    // }
-    title.classList.remove('title');
-    title.classList.add('title1');
-}
-
-setTimeout(callback,2000);
+document.addEventListener("DOMContentLoaded", function(event) { 
+    function callback(){
+        // title.classList.remove('title');
+        // title.classList.add('title1');
+        setInterval(() => {
+            if (title.classList.contains('title1')){
+                title.classList.remove('title1');
+                title.classList.add('title');
+            }else{
+                title.classList.remove('title');
+                title.classList.add('title1');
+            }
+        },2000);
+    }
+    
+    callback();    
+});
