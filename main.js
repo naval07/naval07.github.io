6a64b5f02929cfec926120b3f0cc0b496b4af137
naval07 <49963750+naval07@users.noreply.github.com>
2021-01-05 17:01:46 -0500

    .

--- main.js
+++ main.js
@@ -1,19 +1,19 @@
-let title = document.getElementById('title');
+var title = document.getElementById('title');
 
-title.classList.add('title');
-console.log('clase cambiada?');
-
-function callback(){
-    if (title.classList.contains('title1')){
-        title.classList.remove('title1');
-        title.classList.add('title');
+document.addEventListener("DOMContentLoaded", function(event) { 
+    function callback(){
+        // title.classList.remove('title');
+        // title.classList.add('title1');
+        setInterval(() => {
+            if (title.classList.contains('title1')){
+                title.classList.remove('title1');
+                title.classList.add('title');
+            }else{
+                title.classList.remove('title');
+                title.classList.add('title1');
+            }
+        },2000);
     }
-    if (title.classList.contains('title')){
-        title.classList.remove('title');
-        title.classList.add('title1');
-    }
-    title.classList.remove('title');
-    title.classList.add('title1');
-}
-
-setTimeout(callback,2000);
+    
+    callback();    
+});
